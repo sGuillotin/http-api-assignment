0cb7915791d9890b2aa06c35f3448912088ce8ce
seb <sfg1264@rit.edu>
2026-09-17 19:17:06 -0400

    help from prof, started connecting url endings to status codes, using query parameters. do buttons and client last, XML later.

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -10,7 +10,7 @@ const respondJSON = (request, response, status, object) => {
     if (request.method !== 'HEAD') {
         response.write(content);
     }
-
+    
     response.end;
 };
 
@@ -21,11 +21,30 @@ const notFound = (request, response) => {
         message: 'The page you are looking for was not found.',
         id: 'notFound',
     }
-
+    
     return respondJSON(request, response, 404, responseJSON);
 };
 
+const badRequest = (request, response) => {
+    console.log("bad request called");
+
+    // "interrogate the url"
+    if(request.query.valid === 'true'){
+        const responseJSON = {
+            message: 'Missing valid query parameter set to true.',
+            id: 'badRequest',
+        }
+        return respondJSON(request, response, 400, responseJSON);
+    } else {
+        const responseJSON = {
+            message: 'This request has the valid parameters.',
+        }
+        return respondJSON(request, response, 200, responseJSON);
+    }
+}
+
 
 module.exports = {
-    notFound
+    notFound,
+    badRequest
 };
--- src/server.js
+++ src/server.js
@@ -8,12 +8,17 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const urlStruct = {
   '/': htmlHandler.getIndex,
   '/style.css': htmlHandler.getCSS,
+  '/badRequest': jsonHandler.badRequest,
   notFound: jsonHandler.notFound,
 }
 
 const onRequest = (request, response) => {
   const protocol = request.connection.encrypted ? 'https' : 'http';
   const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+
+  request.query = Object.fromEntries(parsedUrl.searchParams);
+  console.log(parsedUrl.searchParams);
+  console.log(request.query);
 
   if(urlStruct[parsedUrl.pathname]){
     urlStruct[parsedUrl.pathname](request, response);
@@ -25,3 +30,24 @@ const onRequest = (request, response) => {
 http.createServer(onRequest).listen(port, () => {
     console.log(`Listening on 127.0.0.1:${port}`);
 })
+
+
+// Currently using https://github.com/AustinWilloughby/Accept-Header-Status-Code-Spring-2026/blob/master/src/server.js
+
+
+// XML - announcement on mycourses
+/*
+const getCats = (request, response) => {
+  const cat = { name: 'Rhyleigh', age: 20 };
+
+  if (request.acceptedTypes[0] === 'application/xml') {
+    let responseXML = '<response>';
+    responseXML += `<name>${cat.name}</name>`;
+    responseXML += `<age>${cat.age}</age>`;
+    responseXML += '</response>';
+    return respond(request, response, responseXML, 'application/xml');
+  }
+
+  return respond(request, response, JSON.stringify(cat), 'application/json');
+};
+*/
